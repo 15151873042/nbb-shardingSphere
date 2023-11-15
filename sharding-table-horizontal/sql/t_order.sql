3b92481b64c5c13a19201962a8a463a8a38d83dd
胡鹏 <15151873042@163.com>
2023-11-15 18:12:37 +0800

    添加sql

---
+++ sharding-table-horizontal/sql/t_order.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `t_order` (
+                           `id` bigint(20) NOT NULL,
+                           `create_time` datetime DEFAULT NULL,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
